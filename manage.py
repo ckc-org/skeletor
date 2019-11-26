31ea91c3b2fda8c4464a2458ea684eb08d0aef94
Chris Killian <killiantheelder@gmail.com>
2019-11-26 12:25:16 -0800

    Removing a npm-watch subprocess starting with runserver

--- manage.py
+++ manage.py
@@ -7,9 +7,6 @@ if __name__ == "__main__":
     sys.path.append('src')
     settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 
-    if 'runserver' in sys.argv:
-        subprocess.Popen(["npm-watch"])
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
