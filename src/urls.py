0aa7e4b869731b0061145eb5d58e3301b7b7af7a
Chris Killian <killiantheelder@gmail.com>
2019-12-06 11:58:26 -0800

    Added player, binding, and attack to admin

--- src/urls.py
+++ src/urls.py
@@ -7,10 +7,15 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from attacks.models import Attack
+from bindings.models import Binding
 from monster.models import Monsta
+from players.models import Player
 
 admin.site.register(Monsta)
-
+admin.site.register(Player)
+admin.site.register(Binding)
+admin.site.register(Attack)
 
 urlpatterns = [
     # Wagtail Admin and Docs
