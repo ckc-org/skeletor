50bcc7786ff8451ddb05398f9f6bf41be4257a28
ReproKillbot <34966484+ReproKillbot@users.noreply.github.com>
2020-01-23 10:35:26 -0800

    Git Hook (#7)
    * Created a git_hooks folder, added .sh file to create a symlink for every file in it to the local .git/hooks/ folder.
    
    * Updated readme - adding line to use create_symlink.sh
    
    * Added .idea/ to .gitignore, changed create_symlinks.sh => create_git_hooks.sh, indented post-merge
    
    * Removed line going off for each pull
    
    * Fixed error in README - was point to old create_symlinks.sh, now at create_git_hooks.sh
    
    * Create git hooks now makes the hooks directory if it doesn't exist, changes permissions of the post-merge hook
    
    * Changed post-merge to be executable, removed .gitignore line for Pycharm
    
    * Fixed gitignore, removed chmod +x in script
    
    * replaced sh with ./ in readme and changed file perms for create_git_hooks.sh
    
    * Forcing symlink to be overwritten, if it already exists

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ $ django-admin.py startproject something-wonderful --template=https://github.com
 # installation
 
 ```bash
+$ ./create_git_hooks.sh
 $ cp .env_sample .env
 $ docker-compose up -d
 # can now view http://localhost or http://localhost:8000
---
+++ create_git_hooks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+dir=$(pwd)
+hooks_dir=${dir}/git_hooks/
+FILES=${hooks_dir}*
+for f in ${FILES}
+    do
+    mkdir -p .git/hooks/
+    ln -sf "${hooks_dir}${f##*/}" "${dir}/.git/hooks/${f##*/}"
+    echo "Creating symlink for hook(s): ${f##*/}"
+done
