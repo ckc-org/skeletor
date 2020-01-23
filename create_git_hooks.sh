a89e6d379d0e9abd2587ed608032c30ab9465381
Chris Killian <killiantheelder@gmail.com>
2020-01-23 10:16:14 -0800

    Fixed gitignore, removed chmod +x in script

--- create_git_hooks.sh
+++ create_git_hooks.sh
@@ -6,6 +6,5 @@ for f in ${FILES}
     do
     mkdir -p .git/hooks/
     ln -s "${hooks_dir}${f##*/}" "${dir}/.git/hooks/${f##*/}"
-    chmod +x "${hooks_dir}${f##*/}"
     echo "Creating symlink for hook(s): ${f##*/}"
 done
