1f39d970c2734e1a1d1a68a1a76987599f2b09e2
Eric Carmichael <eric@ckcollab.com>
2021-06-24 15:11:57 -0700

    Add nice default user factory

--- src/backend/factories.py
+++ src/backend/factories.py
@@ -2,3 +2,25 @@
 
 https://factoryboy.readthedocs.io/en/latest/recipes.html
 """
+import factory
+
+from django.contrib.auth import get_user_model
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    email = factory.Faker('email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+
+    class Meta:
+        model = get_user_model()
+
+    @factory.post_generation
+    def password(self, created, extracted):
+        if not created:
+            return
+        if extracted:
+            self.set_password(extracted)
+        else:
+            self.set_password('factoryuserpass')
+        self.save()
