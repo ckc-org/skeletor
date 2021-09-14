f44e7a03bc967f2867a049455285c246398d9929
Samuel Wood <samote.wood@gmail.com>
2021-09-14 11:05:36 -0700

    Eslint configs all updated/synced w/ editor config

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -1,29 +1,44 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2020: true
-    },
-    extends: [
-        'plugin:vue/essential',
-    ],
-    parserOptions: {
-        ecmaVersion: 11,
-        sourceType: 'module'
-    },
-    plugins: [
-        'vue'
-    ],
-    ignorePatterns: [
-        '*.md',
-        'node_modules/*',
-        'build/*'
-    ],
-    rules: {
-        // indent: ['off', 4],
-        'func-call-spacing': ['error', 'never'],
-        'vue/script-indent': ['error', 4, {baseIndent: 0}],
-        'vue/valid-v-for': ['off'],
-        'vue/no-use-v-if-with-v-for': ['off'],
-        'vue/require-v-for-key': ['off'],
-    },
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: [
+    'plugin:vue/essential',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  ignorePatterns: ['*.md'],
+  rules: {
+
+    // General
+    indent: ['warn', 2],
+    'no-unused-vars': ['warn'],
+    'func-call-spacing': ['error', 'never'],
+    // "prefer-destructuring": ["error", {
+    //   "array": true,
+    //   "object": true
+    // }, {
+    //   "enforceForRenamedProperties": false
+    // }],
+
+    //Vue
+    'vue/script-indent': ['error', 2, { baseIndent: 0 }],
+    'vue/html-indent': ['error', 2, { baseIndent: 1 }],
+    'vue/valid-v-for': ['warn'],
+    'vue/no-use-v-if-with-v-for': ['warn'],
+    'vue/require-v-for-key': ['error'],
+    'vue/no-arrow-functions-in-watch': ['error'],
+    'vue/no-async-in-computed-properties': ['error'],
+    'vue/no-dupe-keys': ['warn'],
+    'vue/no-dupe-v-else-if': ['warn'],
+    'vue/require-prop-types': ['warn'],
+    'vue/prop-name-casing': ['warn', 'snake_case'],
+    'vue/valid-v-slot': ['off'],
+  },
 }
