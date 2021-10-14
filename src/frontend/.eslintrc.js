2e53e122e9b8995e45142fbd176a7064210278f1
Samuel Wood <samote.wood@gmail.com>
2021-10-14 15:42:09 -0700

    editorconfig + eslint config updated (#84)

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -18,17 +18,26 @@ module.exports = {
 
     // General
     indent: ['warn', 2],
-    'no-unused-vars': ['warn'],
+    "arrow-spacing": ["error", { "before": true, "after": true }],
+    "brace-style": ["error", "1tbs", { "allowSingleLine": false }],
     'func-call-spacing': ['error', 'never'],
-    'object-curly-spacing': ['warn', 'always', { "objectsInObjects": false, "arraysInObjects": false }],
-    "prefer-destructuring": ["error", {
+    'no-unused-vars': ['warn'],
+    "no-mixed-spaces-and-tabs": "error",
+    'object-curly-spacing': ['warn', 'always', { objectsInObjects: false, arraysInObjects: false }],
+    "prefer-const": ["warn"],
+    "prefer-destructuring": ["warn", {
       "array": true,
       "object": true
     }, {
       "enforceForRenamedProperties": false
     }],
+    "space-before-blocks": ["error", "always"],
+    "space-before-function-paren": ["error", "never"],
+    "space-in-parens": ["error", "never"],
+    "template-curly-spacing": ["error", "always"],
 
-    //Vue
+
+    // Vue
     'vue/script-indent': ['error', 2, { baseIndent: 0 }],
     'vue/html-indent': ['error', 2, { baseIndent: 1 }],
     'vue/valid-v-for': ['warn'],
@@ -40,6 +49,6 @@ module.exports = {
     'vue/no-dupe-v-else-if': ['warn'],
     'vue/require-prop-types': ['warn'],
     'vue/prop-name-casing': ['warn', 'snake_case'],
-    'vue/valid-v-slot': ['off'],
-  },
+    'vue/valid-v-slot': ['off']
+  }
 }
