8271dfa82c976d5818a0f759a007c4ca449a1704
Harvey Hartwell <harvey@ckcollab.com>
2022-12-01 08:23:39 -0800

    fix next config

--- src/react_frontend/next.config.js
+++ src/react_frontend/next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/login': { page: '/login' },
-    }
-  },
   env: {
     NEXT_PUBLIC_BACKEND_URL: 'http://localhost:8000'
   },
