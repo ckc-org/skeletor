50f9228384ce472cca911c8e9ccb383fdf98d3a2
gibsonbailey <baileytrefts@gmail.com>
2023-01-20 23:23:21 -0800

    Modify APPEND_SLASH behavior in order to exclude our catch-all template view pattern from checking if a path matches our urlconf.

---
+++ src/backend/apps/utils/middleware.py
@@ -0,0 +1,23 @@
+from django.middleware.common import CommonMiddleware
+from django.conf import settings
+from django.urls import is_valid_path
+
+
+class CustomCommonMiddleware(CommonMiddleware):
+    def should_redirect_with_slash(self, request):
+        """
+        Return True if settings.APPEND_SLASH is True and appending a slash to
+        the request path turns an invalid path into a valid one.
+
+        Exclude our catch-all template view when deciding if a path matches our urlconf.
+        """
+        if settings.APPEND_SLASH and not request.path_info.endswith("/"):
+            urlconf = getattr(request, "urlconf", None)
+            if urlconf is None:
+                urlconf = 'urls.base'
+            if not is_valid_path(request.path_info, urlconf):
+                match = is_valid_path("%s/" % request.path_info, urlconf)
+                if match:
+                    view = match.func
+                    return getattr(view, "should_append_slash", True)
+        return False
--- src/backend/settings/base.py
+++ src/backend/settings/base.py
@@ -52,7 +52,7 @@ MIDDLEWARE = (
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'utils.middleware.CustomCommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
