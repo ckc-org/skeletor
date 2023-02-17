05e97a7fa69091aa28cf148b84744879a285105e
gibsonbailey <baileytrefts@gmail.com>
2023-02-17 14:56:05 -0800

    Use create-expo-app with a typescript template.

--- src/mobile/App.tsx
+++ src/mobile/App.tsx
@@ -1,22 +1,48 @@
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { NativeBaseProvider } from "native-base"
-import { AppRoute, theme } from "./shared"
-import * as Home from "./features/home"
-
-const AppStack = createNativeStackNavigator<AppRoute>()
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NativeBaseProvider
-      theme={theme}
-      config={{ suppressColorAccessibilityWarning: true }}
-    >
-      <NavigationContainer>
-        <AppStack.Navigator screenOptions={{ headerShown: false }}>
-          <AppStack.Screen name="Home" component={Home.HomeScreen} />
-        </AppStack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
-  )
+    <View style={styles.container}>
+      <View style={styles.avatar}>
+      </View>
+      <View style={styles.userInfo}>
+        <Text style={styles.username}>
+          Username
+        </Text>
+        <Text style={styles.username}>
+          Username
+        </Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 9999,
+    backgroundColor: 'gray',
+  },
+  userInfo: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    height: '100%',
+    padding: 10,
+    flex: 1,
+  },
+  username: {
+    display: 'flex',
+  },
+  container: {
+    marginTop: 200,
+    padding: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/mobile/shared/routes.ts
+++
@@ -1,6 +0,0 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack"
-
-export type AppScreenProps<Screen extends keyof AppRoute> = NativeStackScreenProps<AppRoute, Screen>
-export type AppRoute = {
-  Home: undefined
-}
