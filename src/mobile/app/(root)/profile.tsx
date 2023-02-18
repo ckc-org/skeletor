628336a30606c101cf792ba3bbfd4de4b487a77b
gibsonbailey <baileytrefts@gmail.com>
2023-02-17 17:21:00 -0800

    Add react-native-ui-lib

--- src/mobile/app/(root)/profile.tsx
+++ src/mobile/app/(root)/profile.tsx
@@ -1,45 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, TextField, View } from 'react-native-ui-lib';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.avatar}>
+    <View flex paddingH-25 paddingT-120>
+      <Text blue50 text20>Welcome</Text>
+      <TextField text50 placeholder="username" grey10 />
+      <TextField text50 placeholder="password" secureTextEntry grey10 />
+      <View marginT-100 center>
+        <Button text70 white background-orange30 label="Login" />
+        <Button link text70 orange30 label="Sign Up" marginT-20 />
       </View>
-      <View style={styles.userInfo}>
-        <Text style={styles.username}>
-          Username
-        </Text>
-      </View>
-      <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  avatar: {
-    width: 100,
-    height: 100,
-    borderRadius: 9999,
-    backgroundColor: 'gray',
-  },
-  userInfo: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    height: '100%',
-    padding: 10,
-    flex: 1,
-  },
-  username: {
-    display: 'flex',
-  },
-  container: {
-    marginTop: 200,
-    padding: 20,
-    display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
