f91431d13194cb317d5520ed4562015729d27bf9
gibsonbailey <baileytrefts@gmail.com>
2023-02-20 16:45:39 -0800

    Fix background color of stack screens.

--- src/mobile/app/(auth)/_layout.tsx
+++ src/mobile/app/(auth)/_layout.tsx
@@ -11,6 +11,9 @@ export default () => {
         screenOptions={{
           headerShown: !isSplash,
           headerTintColor: 'black',
+          contentStyle: {
+            backgroundColor: 'white',
+          }
         }}
       ></Stack>
     </>
