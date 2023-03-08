623af683c7d873bfd90e38320772535ab0cbee6f
Eric Carmichael <eric@ckcollab.com>
2023-03-08 15:23:49 -0800

    pull unnecessary email settings from heroku .....

--- src/backend/settings/heroku.py
+++ src/backend/settings/heroku.py
@@ -80,19 +80,6 @@ CORS_ORIGIN_WHITELIST = (
 
 
 # =============================================================================
-# Email
-# =============================================================================
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.sendgrid.net')
-EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', "noreply@ckcollab.com")
-SERVER_EMAIL = os.environ.get('SERVER_EMAIL', DEFAULT_FROM_EMAIL)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-
-
-# =============================================================================
 # Logging
 # =============================================================================
 LOGGING['handlers']['mail_admins'] = {
