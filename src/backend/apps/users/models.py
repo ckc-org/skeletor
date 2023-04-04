e721a19060d7568ba47e9d2bec764aff570e6320
Eric Carmichael <eric@ckcollab.com>
2023-04-03 20:27:59 -0700

    f8

--- src/backend/apps/users/models.py
+++ src/backend/apps/users/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.postgres.fields import CIEmailField
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
