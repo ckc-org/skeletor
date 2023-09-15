74c485fc3f80927f7dadd7a701c53df46ea7394e
Samuel Wood <samote.wood@gmail.com>
2023-09-14 18:10:14 -0700

    General ErrorHandler composable set up

---
+++ src/frontend/composables/useErrorHandler.ts
@@ -0,0 +1,29 @@
+import {useSnackbarStore} from "~/store/snackbar";
+
+const useErrorHandler = (err) => {
+    const snackbar = useSnackbarStore();
+    if (err?.data) {
+        if (err?.data?.non_field_errors) {
+            snackbar.displaySnackbar('error', err?.data?.non_field_errors)
+        } else if (typeof err?.data[0] === 'string') {
+
+            if (+err?.status >= 500) {
+                snackbar.displaySnackbar('error', 'Something went wrong. Our team has been notified.',)
+            } else {
+                if (Array.isArray(err?.data)) {
+                    snackbar.displaySnackbar('error', err?.data[0])
+                } else {
+                    const msg = JSON.stringify(err?.data);
+                    snackbar.displaySnackbar('error', msg.length < 200 ? msg : 'Unexpected error')
+                }
+            }
+        } else {
+            // Catch all for anything (detail not found, etc.)
+            snackbar.displaySnackbar('error', 'Something went wrong.',)
+        }
+    } else {
+        snackbar.displaySnackbar('error', err.message);
+    }
+};
+
+export default useErrorHandler;
