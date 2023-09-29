9fe3b02dfda6d04276c9ab75b5e7c219c7327b4e
Samuel Wood <samote.wood@gmail.com>
2023-09-29 08:36:19 -0700

    feat: adding snackbar

---
+++ src/frontend/store/snackbar.ts
@@ -0,0 +1,30 @@
+// snackbarStore.ts
+import {defineStore} from 'pinia';
+
+interface SnackbarState {
+  message: string;
+  color: string;
+  show: boolean;
+}
+
+export const useSnackbarStore = defineStore({
+  id: 'snackbar',
+  state: (): SnackbarState => ({
+    message: '',
+    color: '',
+    show: false
+  }),
+  actions: {
+    displaySnackbar(color: string, message: string) {
+      if (color === 'error') {
+        console.error(message)
+      }
+      this.color = color;
+      this.message = message;
+      this.show = true;
+    },
+    closeSnackbar() {
+      this.show = false;
+    }
+  }
+});
