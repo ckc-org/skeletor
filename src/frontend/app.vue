c673d7eb7b69ede0a194808a34367d1994be7648
Samuel Wood <samote.wood@gmail.com>
2023-10-08 14:28:34 -0700

    fix: fixed snackbar not showing up

--- src/frontend/app.vue
+++ src/frontend/app.vue
@@ -2,6 +2,7 @@
   <div>
     <NuxtLayout>
       <NuxtPage/>
+      <Snackbar />
     </NuxtLayout>
   </div>
 </template>
--- src/frontend/composables/useErrorHandler.ts
+++ src/frontend/composables/useErrorHandler.ts
@@ -1,29 +1,29 @@
 import {useSnackbarStore} from "~/store/snackbar";
 
 const useErrorHandler = (err) => {
-    const snackbar = useSnackbarStore();
-    if (err?.data) {
-        if (err?.data?.non_field_errors) {
-            snackbar.displaySnackbar('error', err?.data?.non_field_errors)
-        } else if (typeof err?.data[0] === 'string') {
+  const snackbar = useSnackbarStore();
+  if (err?.data) {
+    if (err?.data?.non_field_errors) {
 
-            if (+err?.status >= 500) {
-                snackbar.displaySnackbar('error', 'Something went wrong. Our team has been notified.',)
-            } else {
-                if (Array.isArray(err?.data)) {
-                    snackbar.displaySnackbar('error', err?.data[0])
-                } else {
-                    const msg = JSON.stringify(err?.data);
-                    snackbar.displaySnackbar('error', msg.length < 200 ? msg : 'Unexpected error')
-                }
-            }
+      snackbar.displaySnackbar('error', err?.data?.non_field_errors[0])
+    } else if (typeof err?.data[0] === 'string') {
+      if (+err?.statusCode >= 500 || +err?.status >= 500) {
+        snackbar.displaySnackbar('error', 'Something went wrong. Our team has been notified.',)
+      } else {
+        if (Array.isArray(err?.data)) {
+          snackbar.displaySnackbar('error', err?.data[0])
         } else {
-            // Catch all for anything (detail not found, etc.)
-            snackbar.displaySnackbar('error', 'Something went wrong.',)
+          const msg = JSON.stringify(err?.data);
+          snackbar.displaySnackbar('error', msg.length < 200 ? msg : 'Unexpected error')
         }
+      }
     } else {
-        snackbar.displaySnackbar('error', err.message);
+      // Catch all for anything (detail not found, etc.)
+      snackbar.displaySnackbar('error', 'Something went wrong.',)
     }
+  } else {
+    snackbar.displaySnackbar('error', err.message);
+  }
 };
 
 export default useErrorHandler;
