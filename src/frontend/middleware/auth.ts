70fcad648364b25d76c732d45eb57bae82148da9
Samuel Wood <samote.wood@gmail.com>
2023-10-22 15:03:12 -0700

    Remove cruft from auth/guest middlewares

--- src/frontend/middleware/auth.ts
+++ src/frontend/middleware/auth.ts
@@ -1,6 +1,3 @@
-import {UserT, UserWithoutPasswordT} from "~/types/authTypes";
-import {useSnackbarStore} from "~/store/snackbar";
-import {RouteLocationNormalized} from "vue-router";
 import {userStore} from "~/store/user";
 
 export default defineNuxtRouteMiddleware(async (_to, _from) => {
@@ -14,27 +11,10 @@ export default defineNuxtRouteMiddleware(async (_to, _from) => {
         // Wait for fetch to complete
         await user.$state.fetchUserHasBeenRan
 
-        // User is now fetched, proceed with middleware checks
-          console.log("shes thurr")
-
       } catch {
         // Handle error - redirect to login
       }
     }
-
-    // const csrftoken = useCookie('csrftoken')
-    // const sessionid = useCookie('sessionid').value
-    // const hasSession = Boolean(sessionid)
-    //
-    // console.log(`User has session: ${hasSession}, sessionid: ${sessionid}`)
-    //
-    // if(!hasSession){
-    //     console.log("User is not logged in, redirecting to login page")
-    //     return navigateTo('/')
-    // }
-
-
-    // const user = userStore()
 
     // If the user is not logged in, redirect to the login page
     if (!user.isLoggedIn) {
@@ -42,11 +22,6 @@ export default defineNuxtRouteMiddleware(async (_to, _from) => {
         return navigateTo('/')
     }
 
-    console.log("User is logged in, continuing to dashboard")
+    console.log("User is logged in, continuing to route")
 
-
-    // const redirectRoute = getUserRedirectPath(auth.authUser, _to, _from)
-    // if (redirectRoute !== _to.path && redirectRoute) {
-    //   return navigateTo('/dashboard')
-    // }
 })
