fc664bbf0994dd310be5d0ffc8481109db90e50d
Samuel Wood <samote.wood@gmail.com>
2023-10-22 15:02:07 -0700

    Cleaned to app.vue

--- src/frontend/app.vue
+++ src/frontend/app.vue
@@ -1,44 +1,38 @@
 <template>
-  <div>
-    <div v-if="isLoading" class="loading">
-      <!-- loading text? -->
-    </div>
-    <div v-else class="content">
-      <NuxtLayout>
-        <NuxtPage/>
-        <Snackbar />
-      </NuxtLayout>
-    </div>
+  <div class="fade-in">
+    <v-sheet
+      v-if="isLoading"
+      class="d-flex justify-center align-center"
+      height="90vh"
+    >
+      <v-progress-circular indeterminate />
+    </v-sheet>
+    <NuxtLayout v-else>
+      <NuxtPage/>
+      <Snackbar/>
+    </NuxtLayout>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { userStore } from "~/store/user";
+import { ref, onMounted } from 'vue'
+import { userStore } from "~/store/user"
 
 const user = userStore()
-const isLoading = ref(true);  // Added loading state
+const isLoading = ref(true)  // Added loading state
 
 onMounted(async () => {
   // Fetch the user's data, the page redirects and such depend on this data
   // before continuing
   try {
     await user.fetchUser()
-  } catch(e) {
-    // We don't care?!
+  } catch (e) {
+    // We don't care if this fails, it just means the user isn't logged in
+  } finally {
+    isLoading.value = false  // Update loading state after fetching the user
   }
-
-  isLoading.value = false;  // Update loading state after fetching the user
-});
+})
 </script>
 
 <style scoped lang="stylus">
-.loading, .content
-  transition opacity 0.5s ease-in-out
-
-.loading
-  opacity 0
-
-.content
-  opacity 1
 </style>
