57de7857dc1e55679dc3f455192cc8e26d3ea318
Samuel Wood <samote.wood@gmail.com>
2024-01-12 07:56:33 -0800

    committing unused var

--- bin/linting/lint_frontend.sh
+++ bin/linting/lint_frontend.sh
@@ -1,1 +1,10 @@
 eslint "$@" --ext .js,.vue,.ts --ignore-path .gitignore
+eslint_status=$?
+
+# Check the exit status of ESLint
+if [ $eslint_status -ne 0 ]; then
+    echo "Linting errors detected"
+    exit 1
+fi
+
+exit 0
