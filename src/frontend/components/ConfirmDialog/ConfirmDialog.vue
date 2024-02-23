6999750dd69e395697d24d21c3f740ce623ef0be
hhartwell <harvey@ckcollab.com>
2024-02-23 13:06:48 -0800

    confirm dialog (#191)
    * confirm dialog
    
    * fix dialog
    
    * Update src/frontend/components/ConfirmDialog/ConfirmDialog.vue
    
    Co-authored-by: Brian Currie <brianryleycurrie@gmail.com>
    
    ---------
    
    Co-authored-by: Brian Currie <brianryleycurrie@gmail.com>

---
+++ src/frontend/components/ConfirmDialog/ConfirmDialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-dialog v-model="value" @blur="emit('cancel')">
+    <v-card>
+      <v-card-title>
+        {{ title }}
+      </v-card-title>
+      <v-card-text>
+        {{ text }}
+      </v-card-text>
+      <v-card-actions class="d-flex justify-end align-center">
+        <v-btn @click="emit('cancel')">{{ cancelText }}</v-btn>
+        <v-btn @click="emit('confirm')" color="primary">{{ confirmText }}</v-btn>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+const props = defineProps({
+  value: {
+    type: Boolean,
+    required: true
+  },
+  title: {
+    type: String,
+    default: 'Are you sure?'
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+  confirmText: {
+    type: String,
+    default: 'OK'
+  },
+  cancelText: {
+    type: String,
+    default: 'Cancel'
+  },
+})
+
+const emit = defineEmits(['cancel', 'confirm'])
+
+</script>
+<style scoped lang="stylus">
+
+</style>
