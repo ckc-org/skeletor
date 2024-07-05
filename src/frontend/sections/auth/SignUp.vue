dc188a20c9d1f13dc089db5290db9867fe76b7f1
Eric Carmichael <eric@ckcollab.com>
2024-07-05 16:56:49 -0700

    Better signup

--- src/frontend/sections/auth/SignUp.vue
+++ src/frontend/sections/auth/SignUp.vue
@@ -4,35 +4,49 @@
 
   <VForm class="mt-7" @submit.prevent="submit">
     <div>
-      <label class="label text-grey-darken-2" for="name">Name</label>
+      <label class="label text-grey-darken-2" for="name">First Name</label>
       <VTextField
-        id="name"
-        v-model="name"
-        :rules="[ruleRequired]"
+        id="first_name"
+        v-model="form.first_name"
         prepend-inner-icon="fluent:person-24-regular"
-        name="name"
+        name="first_name"
+        :error-messages="errors.first_name"
+        :error="!!errors.first_name"
+      />
+    </div>
+    <div>
+      <label class="label text-grey-darken-2" for="name">Last Name</label>
+      <VTextField
+        id="last_name"
+        v-model="form.last_name"
+        prepend-inner-icon="fluent:person-24-regular"
+        name="last_name"
+        :error-messages="errors.last_name"
+        :error="!!errors.last_name"
       />
     </div>
     <div class="mt-1">
       <label class="label text-grey-darken-2" for="email">Email</label>
       <VTextField
         id="email"
-        v-model="email"
-        :rules="[ruleRequired, ruleEmail]"
+        v-model="form.email"
         type="email"
         prepend-inner-icon="fluent:mail-24-regular"
         name="email"
+        :error-messages="errors.email"
+        :error="!!errors.email"
       />
     </div>
     <div class="mt-1">
       <label class="label text-grey-darken-2" for="password">Password</label>
       <VTextField
         id="password"
-        v-model="password"
-        :rules="[ruleRequired, rulePassLen]"
+        v-model="form.password"
         type="password"
         prepend-inner-icon="fluent:password-20-regular"
         name="password"
+        :error-messages="errors.password"
+        :error="!!errors.password"
       />
     </div>
     <div class="mt-5">
@@ -42,19 +56,32 @@
     </div>
   </VForm>
   <p class="text-body-2 mt-10">
-    <span
-      >Already have an account?
+    <span>
+      Already have an account?
       <NuxtLink to="/" class="font-weight-bold text-primary">Sign In</NuxtLink>
     </span>
   </p>
 </template>
 
 <script setup>
-const name = ref("");
-const email = ref("");
-const password = ref("");
+import { userStore } from "~/store/user";
 
-const { ruleEmail, rulePassLen, ruleRequired } = useFormRules();
+const user = userStore();
 
-const submit = async () => {};
+const form = ref({
+  first_name: "",
+  last_name: "",
+  email: "",
+  password: "",
+});
+const errors = ref({});
+
+const submit = async () => {
+  try {
+    await user.createUser(form.value);
+    navigateTo("/dashboard");
+  } catch (e) {
+    errors.value = e.data || {};
+  }
+};
 </script>
