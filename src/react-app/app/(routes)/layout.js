2450bf29d0b5bcedbd1dec3207db20115e1c6be7
Samuel Wood <samote.wood@gmail.com>
2024-12-02 12:43:38 -0800

    testing linting

--- src/react-app/app/(routes)/layout.js
+++ src/react-app/app/(routes)/layout.js
@@ -1,12 +1,12 @@
-"use client";
+"use client"
 
-import { ThemeProvider } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import "../../styles/globals.css";
-import theme from "../../theme";
-import { Providers } from "./providers";
+import { ThemeProvider } from "@mui/material"
+import CssBaseline from "@mui/material/CssBaseline"
+import "../../styles/globals.css"
+import theme from "../../theme"
+import { Providers } from "./providers"
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, ...props }) {
   return (
     <html lang="en">
       <body>
@@ -18,5 +18,5 @@ export default function RootLayout({ children }) {
         </Providers>
       </body>
     </html>
-  );
+  )
 }
